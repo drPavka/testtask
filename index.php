b9f19b45938233bd4743759e9a21132d257b118c
Pavlo Dubenko <d.pavka@gmail.com>
2020-08-20 16:42:40 +0300

    basic implementation

--- index.php
+++ index.php
@@ -1,9 +1,38 @@
-# load calendar here
+<html>
+<head>
+    <meta charset='utf-8'/>
+    <link href='fullcalendar-4.4.0/packages/core/main.css' rel='stylesheet'/>
+    <link href='fullcalendar-4.4.0/packages/daygrid/main.css' rel='stylesheet'/>
+    <link href='fullcalendar-4.4.0/packages/timegrid/main.css' rel='stylesheet'/>
 
+    <script src='fullcalendar-4.4.0/packages/core/main.js'></script>
+    <script src='fullcalendar-4.4.0/packages/daygrid/main.js'></script>
+    <script src='fullcalendar-4.4.0/packages/timegrid/main.js'></script>
+    <script>
+        (function () {
+            const loader = function () {
+                const calendarEl = document.getElementById('calendar');
 
-# api endpoint:
-api/index.php
+                const calendar = new FullCalendar.Calendar(calendarEl, {
+                    plugins: ['timeGrid'],
+                    defaultView: 'timeGridWeek'
+                });
 
+                calendar.render();
+                calendar.addEvent({
+                    start: '2020-05-10 08:00:00'
+                    end: '2020-05-10 10:00:00'
+                    title: 'Meeting'
+                    eventId: 1
+                })
+            }
 
-# docs
-https://fullcalendar.io/docs/getting-started
+            document.addEventListener('DOMContentLoaded', loader);
+        })();
+
+    </script>
+</head>
+<body>
+<div id="calendar"></div>
+</body>
+</html>
