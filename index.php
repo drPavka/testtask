b64c01c659c6ee5a6f2db91ca023586be41ddc3e
Pavlo Dubenko <d.pavka@gmail.com>
2020-08-20 16:43:44 +0300

    final

--- index.php
+++ index.php
@@ -1,33 +1,115 @@
+<?php
+define( 'CALENDAR_PATH', 'fullcalendar-4.4.0/packages' );
+define( 'API_PATH', '/api' );
+function importCalendarLibraries( array $dirs, string $pattern ): string {
+	return join(
+		array_map(
+			function ( $package ) use ( $pattern ) {
+				return sprintf( $pattern, CALENDAR_PATH . '/' . $package );
+			},
+			$dirs
+		)
+	);
+}
+
+?>
 <html>
 <head>
     <meta charset='utf-8'/>
-    <link href='fullcalendar-4.4.0/packages/core/main.css' rel='stylesheet'/>
-    <link href='fullcalendar-4.4.0/packages/daygrid/main.css' rel='stylesheet'/>
-    <link href='fullcalendar-4.4.0/packages/timegrid/main.css' rel='stylesheet'/>
+    <style>
+        * {
+            box-sizing: border-box;
+        }
 
-    <script src='fullcalendar-4.4.0/packages/core/main.js'></script>
-    <script src='fullcalendar-4.4.0/packages/daygrid/main.js'></script>
-    <script src='fullcalendar-4.4.0/packages/timegrid/main.js'></script>
+        html, body {
+            width: 100%;
+            height: 100%;
+            padding: 0;
+            margin: 0;
+        }
+
+        #calendar {
+            width: 100%;
+            height: 100%;
+        }
+    </style>
+	<?= importCalendarLibraries(
+		[
+			'core',
+			'daygrid',
+			'timegrid',
+		],
+		'<link href="%s/main.css" rel="stylesheet"/>'
+	) ?>
+	<?= importCalendarLibraries(
+		[
+			'core',
+			'daygrid',
+			'timegrid',
+			'interaction',
+		],
+		'<script src="%s/main.js"></script>'
+	) ?>
     <script>
+
         (function () {
             const loader = function () {
-                const calendarEl = document.getElementById('calendar');
+                try {
+                    const createCalendar = function () {
+                        const calendarEl = document.getElementById('calendar');
+                        if (!calendarEl) throw new Error('Calendar container is not set');
+                        const onChange = function ({event, revert}) {
+                            window.fetch('<?= API_PATH ?>', {
+                                method: 'POST',
+                                headers: {
+                                    'Content-Type': 'application/json'
+                                },
+                                body: JSON.stringify({
+                                    start: event.start,
+                                    end: event.end,
+                                    id: event.id,
+                                    scope: 'update'
+                                })
+                            }).then((response) => {
+                                if (!response.ok) throw new Error(response.statusText);
+                            }).catch((e) => {
+                                //console.error(e);
+                                revert();
+                            });
+                        };
+                        const _ = new FullCalendar.Calendar(calendarEl, {
+                            plugins: ['timeGrid', 'interaction',],
+                            defaultView: 'timeGridWeek',
+                            editable: true,
+                            selectable: true,
+                            eventColor: '#fff',
+                            eventBackgroundColor: '#000',
+                            eventDrop: onChange,
+                            eventResize: onChange
+                        });
+                        _.render();
+                        return _;
+                    }
 
-                const calendar = new FullCalendar.Calendar(calendarEl, {
-                    plugins: ['timeGrid'],
-                    defaultView: 'timeGridWeek'
-                });
+                    const populateEvent = function (calendar, eventData) {
+                        calendar.addEvent(eventData);
+                        calendar.gotoDate(calendar.getEventById(eventData.id).start);
+                    }
 
-                calendar.render();
-                calendar.addEvent({
-                    start: '2020-05-10 08:00:00'
-                    end: '2020-05-10 10:00:00'
-                    title: 'Meeting'
-                    eventId: 1
-                })
+                    const calendar = createCalendar();
+                    populateEvent(calendar, {
+                        start: '2020-05-10 08:00:00',
+                        end: '2020-05-10 10:00:00',
+                        title: 'Meeting',
+                        id: 1
+                    });
+                } catch (e) {
+                    console.error(e)
+                }
             }
 
             document.addEventListener('DOMContentLoaded', loader);
+
         })();
 
     </script>
